172b8bfba4b1cd05235dc58e1e497cfb57527d07
Adrien <Adnn@users.noreply.github.com>
2021-02-10 16:40:31 +0100

    Release branch is now an explicit argument

--- admin/release.py
+++ admin/release.py
@@ -49,27 +49,19 @@ def bump_changelog(v):
     replace_content("CHANGELOG.md", filedata)
 
 
-def commit_and_tag(v):
+def commit_and_tag(v, release_branch):
     subprocess.run(["git", "add", "CHANGELOG.md"], check=True)
     subprocess.run(["git", "commit", "-m", "Prepare release v{}".format(v)], check=True)
-    branches = subprocess.run(["git", "branch", "--list"], check=True, capture_output=True).stdout.decode("utf-8")
-
-    if re.match("\smain^", branches, flags=re.MULTILINE):
-        release_branch = "main"
-    elif re.match("\smaster^", branches, flags=re.MULTILINE):
-        release_branch = "master"
-    else:
-        raise Exception("Cannot infer the release branch, please merge and tag it manually")
-
     subprocess.run(["git", "checkout", release_branch], check=True)
     subprocess.run(["git", "merge", "develop", "--no-ff"], check=True)
-    subprocess.run(["git", "tag", "-a", "v{}".format(version), "-m", "Release v{}".format(version)], check=True)
+    subprocess.run(["git", "tag", "-a", "v{}".format(v), "-m", "Release v{}".format(v)], check=True)
     subprocess.run(["git", "push"], check=True)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Prepare a release by updating the changelog and commiting changes to current branch + tagging a new commit on 'main'")
     parser.add_argument("version", help="The version to be released")
+    parser.add_argument("releasebranch", help="The branch into which the releases are tagged")
 
     args = parser.parse_args()
 
@@ -81,4 +73,4 @@ if __name__ == "__main__":
 
     v = version.Version(args.version)
     bump_changelog(v)
-    commit_and_tag(v)
+    commit_and_tag(v, args.releasebranch)
