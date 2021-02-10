cd90746cd025834085798a413b7f92b6f27589d5
Adrien <Adnn@users.noreply.github.com>
2021-02-10 16:04:16 +0100

    WIP: admin script to release while handling keep-a-changelog

---
+++ admin/release.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+import argparse
+from datetime import date
+from packaging import version
+import re
+import subprocess
+
+
+def filecontent(filepath):
+    with open(filepath, "r") as file :
+      return file.read()
+
+
+def replace_content(filepath, filedata):
+    with open(filepath, "w") as file:
+      file.write(filedata)
+
+
+def previous_version(content):
+    matches = re.search("## \[(.+)] -", content)
+    return version.Version(matches.group(1)) if matches else None
+
+
+def comparison_url(previous, current=None):
+    origin_remote = subprocess.check_output(["git", "remote", "get-url", "origin"]).decode('utf-8')
+    base = re.search("git@github.com:(.+)\.git", origin_remote).group(1)
+    current = "v{}".format(current) if current else "HEAD"
+    if previous:
+        return "https://github.com/{}/compare/v{}...{}".format(base, previous, current)
+    elif current:
+        return "https://github.com/{}/releases/tag/{}".format(base, current)
+    else:
+        raise Exception("Previous and current cannot both be 'None' at the same time")
+
+
+def bump_changelog(v):
+    filedata = filecontent("CHANGELOG.md")
+    previous_v = previous_version(filedata)
+    if previous_v and (v <= previous_v):
+        raise Exception("Version numbers MUST increment")
+
+    release_replacement = "## [Unreleased]\n\n## [{}] - {}".format(v, date.today())
+    filedata = filedata.replace("## [Unreleased]", release_replacement)
+
+    link_replacement = "[Unreleased]: {}\n[{}]: {}".format(comparison_url(v), v, comparison_url(previous_v, v))
+    filedata = re.sub("^\[Unreleased]:.*$", link_replacement, filedata, flags=re.MULTILINE)
+
+    replace_content("CHANGELOG.md", filedata)
+
+
+def commit_and_tag(v):
+    subprocess.run(["git", "add", "CHANGELOG.md"], check=True)
+    subprocess.run(["git", "commit", "-m", "Prepare release v{}".format(v)], check=True)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Prepare a release by updating the changelog and commiting changes to current branch + tagging a new commit on 'main'")
+    parser.add_argument("version", help="The version to be released")
+
+    args = parser.parse_args()
+
+    if subprocess.run(["git", "diff-index", "--quiet", "HEAD"]).returncode != 0:
+        raise Exception("Repository has changes, please stash them first")
+
+    v = version.Version(args.version)
+    bump_changelog(v)
+    commit_and_tag(v)
